60c7c7cbb4f60340843fe7e987fec825933636b6
Sergey Khaladji <khaladji2017@gmail.com>
2021-03-25 10:18:05 +0200

    Refactored the API routing module taking into account filtering in Get-requests

--- src/appRouter.ts
+++ src/appRouter.ts
@@ -4,47 +4,29 @@ import {ICashier} from './model';
 
 export const appRouter = express.Router();
 
-const handler = (f: Function) => async (req: Request, res: Response) => {
-	f(async (err: Error, cashiers: ICashier[]) => {
+const callback = (res: Response) => (err: Error, rowCount: Number) =>
+  err ?	res.status(500).json({"message": err.message}):
+		res.status(200).json({"rowCount": rowCount});
+
+appRouter.get("/", (req: Request, res: Response) => {
+	getCashier((err: Error, cashiers: ICashier[]) => {
 		if (err)
 			res.status(500).json({"errorMessage": err.message})
 		else {
-			res.status(200).json({"data": cashiers});
-			await console.table(cashiers);
+			res.status(200).json({"cashiers": cashiers});
+			console.table(cashiers);
 		}
-	}, req.params.id);
-};
-
-// Create a cashier
-appRouter.post("/", async (req: Request, res: Response) => {
-  createCashier(<ICashier> req.body, (err: Error, cashierId: number) => {
-    if (err) {
-      return res.status(500).json({"message": err.message});
-    }
-	console.table(cashierId);
-    res.status(200).json({"cashierId": cashierId});
-  });
-});
-// http://localhost:3000/cashiers
-// Read a cashier
-appRouter.get("/:id", handler(getCashier));
-
-// Update a cashier
-appRouter.put("/:id", async (req: Request, res: Response) => {
-  updateCashier(<ICashier> req.body, (err: Error, cashierId: number) => {
-    if (err) {
-      return res.status(500).json({"message": err.message});
-    }
-    res.status(200).json({"cashierId": cashierId});
-  }, Number(req.params.id));
+	}, req.query);
 });
 
-// Delete a cashier
-appRouter.delete("/:id", async (req: Request, res: Response) => {
-  deleteCashier(Number(req.params.id), (err: Error, cashierId: number) => {
-    if (err) {
-      return res.status(500).json({"message": err.message});
-    }
-    res.status(200).json({"cashierId": cashierId});
-  });
+appRouter.post("/", (req: Request, res: Response) => {
+	createCashier(<ICashier> req.body, callback(res));
+});
+
+appRouter.put("/:id", (req: Request, res: Response) => {
+	updateCashier(<ICashier> req.body, callback(res), Number(req.params.id));
+});
+
+appRouter.delete("/:id", (req: Request, res: Response) => {
+	deleteCashier(Number(req.params.id), callback(res));
 });
