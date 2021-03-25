5b9d9dbd0ab6a69a11983700f129b7c6583ddf70
Sergey Khaladji <khaladji2017@gmail.com>
2021-03-25 09:53:27 +0200

    API routing module and PostgreSQL database module are connected to the application. Call getAllCashiers + getTargetCashiers1 + getTargetCashiers2.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, {Application, Request, Response, NextFunction} from 'express';
+import {appRouter} from "./appRouter";
 import * as bodyParser from "body-parser";
 import * as dotenv from "dotenv";
+import {getTargetCashiers1, getTargetCashiers2, getAllCashiers} from './db';
 
 dotenv.config();
 const port = Number(process.env.PORT || 3000);
@@ -12,7 +14,12 @@ const logRequest = (req: Request, res: Response, next: NextFunction) => {
 const app: Application = express();
 app.use(logRequest);
 app.use(bodyParser.json());
+app.use("/cashiers", appRouter);
 
 app.listen(port, () => {
     console.log('Server started on port %s', port);
 });
+
+getAllCashiers();
+getTargetCashiers1(1);
+getTargetCashiers2();
